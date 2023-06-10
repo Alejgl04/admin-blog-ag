3c1361ccc83de4d1ae0e73c4c75526873ddb755c
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2023-06-10 18:14:36 -0400

    table reponsive

--- resources/views/livewire/admin/posts-index.blade.php
+++ resources/views/livewire/admin/posts-index.blade.php
@@ -5,34 +5,51 @@
 
     @if ( $posts->count() )
         <div class="card-body">
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Nanme</th>
-                        <th colspan="2"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($posts as $post)
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead>
                         <tr>
-                            <td> {{ $post->id }} </td>
-                            <td> {{ $post->name }} </td>
-                            <td with="10px">
-                                <a class="btn btn-primary btn-sm" href="{{ route('admin.posts.edit', $post) }}"> Update </a>
-                            </td>
-                            <td with="10px">
-                                <form action="{{ route('admin.posts.destroy', $post)}}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-
-                                    <button class="btn btn-danger btn-sm" type="submit">Remove</button>
-                                </form>
-                            </td>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Category</th>
+                            <th>Tags</th>
+                            <th>Created At</th>
+                            <th colspan="2"></th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @foreach ($posts as $post)
+                            <tr>
+                                <td> {{ $post->id }} </td>
+                                <td> {{ $post->name }} </td>
+                                <td> {{ $post->category->name }} </td>
+                                <td>
+                                    @foreach ($post->tags as $tag)
+                                    <small>
+                                        {{ $tag->name }}
+                                    </small>
+
+                                    @endforeach
+                                </td>
+                                <td>
+                                    {{ $post->created_at->format('d/m/Y') }}
+                                </td>
+                                <td with="10px">
+                                    <a class="btn btn-primary btn-sm" href="{{ route('admin.posts.edit', $post) }}"> Update </a>
+                                </td>
+                                <td with="10px">
+                                    <form action="{{ route('admin.posts.destroy', $post)}}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+
+                                        <button class="btn btn-danger btn-sm" type="submit">Remove</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
 
         <div class="card-footer">
