9c9a99457b792df0e206e030097bedfce87b43f7
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2023-06-10 15:56:21 -0400

    post image

--- resources/views/components/card-post.blade.php
+++ resources/views/components/card-post.blade.php
@@ -1,6 +1,11 @@
 @props(['post'])
 <article class="mb-4 mt-2 bg-white shadow-lg rounded-lg overflow-hidden">
-    <img class="w-full object-cover object-center" src="{{ Storage::url( $post->image->url ) }}" alt="{{ $post->name }}" style="height: 30rem;">
+
+    @if ( $post->image )
+        <img class="w-full object-cover object-center" src="{{ Storage::url( $post->image->url ) }}" alt="{{ $post->name }}" style="height: 30rem;">
+    @else
+        <img class="w-full object-cover object-center" src="/images/landspace.jpg" alt="landspace" style="height: 30rem;">
+    @endif
 
     <div class="px-6 py-8">
         <h1 class="font-bold text-xl mb-2">
@@ -8,7 +13,7 @@
         </h1>
 
         <div class="text-gray-700 text-base">
-            {{ $post->extract }}
+            {!!$post->extract!!}
         </div>
     </div>
     <div class="px-6 pb-2">
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,16 +4,20 @@
             {{ $post->name }}
         </h1>
         <div class="text-lg text-gray-500 mb-2">
-            {{ $post->extract }}
+            {!!$post->extract!!}
         </div>
         <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
             <div class="lg:col-span-2">
                 {{-- Contenido principal --}}
                 <figure>
-                    <img class="w-full h-80 object-cover object-center" src="{{Storage::url( $post->image->url )}}" alt="{{ $post->name }}">
+                    @if ( $post->image )
+                    <img class="w-full object-cover object-center" src="{{ Storage::url( $post->image->url ) }}" alt="{{ $post->name }}" style="height: 30rem;">
+                    @else
+                        <img class="w-full object-cover object-center" src="/images/landspace.jpg" alt="landspace" style="height: 30rem;">
+                    @endif
                 </figure>
                 <div class="text-base text-gray-500 mt-4">
-                    {{ $post->body }}
+                    {!!$post->body!!}
                 </div>
             </div>
             <aside>
@@ -23,7 +27,11 @@
                     @foreach ($similars as $similar)
                         <li class="mb-4">
                             <a href="{{ route('posts.show', $similar)}}">
-                                <img class="w-36 h-20" src="{{ storage::url( $similar->image->url )}}" alt="{{ $similar->name }}">
+                                @if ( $post->image )
+                                    <img class="w-36 h-20" src="{{ Storage::url( $similar->image->url ) }}" alt="{{ $similar->name }}" style="height: 30rem;">
+                                @else
+                                    <img class="w-36" src="/images/landspace.jpg" alt="landspace">
+                                @endif
                                 <span class="ml-1 text-gray-600">{{ $similar->name }}</span>
                             </a>
                         </li>
